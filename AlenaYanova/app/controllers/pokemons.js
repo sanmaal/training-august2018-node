5afd17ee2904724da7b6fe44c89633f915bb9dda
Alena Ianova <alena_ianova@epam.com>
2018-08-21 00:25:28 +0300

    filter unnecessary data from pokemons routs, refactoring

--- AlenaYanova/app/controllers/pokemons.js
+++ AlenaYanova/app/controllers/pokemons.js
@@ -4,13 +4,11 @@ const
 
 exports.getPokemonById = (req, res, next) => {
   const details = { 'id': req.params.id };
-  Pokemon.findOne(details, (err, item) => {
-    if (err) {
-      return next(err);
-    } else {
-      res.status(200).send(item);
-    }
-  });
+  Pokemon.findOne(details)
+    .select({ 'catchInfo.userId': 0, '_id': 0, '__v': 0 })
+    .then(pokemon =>
+      res.status(200).send(pokemon))
+    .catch(err => next(err))
 };
 
 exports.pagination = (req, res, next) => {
@@ -18,15 +16,16 @@ exports.pagination = (req, res, next) => {
     query = url_parse(req.url, true).query,
     perPage = query.perPage? +query.perPage: 10,
     page = Math.max(0, req.params.page);
-  Pokemon.find(req.query_details)
+  Pokemon.find(req.details)
     .skip(perPage * (page - 1))
     .limit(perPage)
     .sort('id')
+    .select({ 'catchInfo.userId': 0, '_id': 0, '__v': 0 })
     .exec((err, pokemons) => {
       if (err) {
         return next(err);
       } else {
-      Pokemon.countDocuments(req.query_details).exec((err, count) =>{
+      Pokemon.countDocuments(req.details).exec((err, count) =>{
         if (err) {
           return next(err);
         }
@@ -39,13 +38,13 @@ exports.pagination = (req, res, next) => {
     });
 };
 
-exports.getPokemons = (req, res, next) => {
-  req.query_details = {};
+exports.setPokemonsQuery = (req, res, next) => {
+  req.details = {};
   next();
 };
 
-exports.getCaughtPokemons = (req, res, next) => {
-  req.query_details = {
+exports.setCaughtPokemonsQuery = (req, res, next) => {
+  req.details = {
     'catchInfo.isCaught': true,
     'catchInfo.userId': req.user._id
   };
@@ -53,17 +52,15 @@ exports.getCaughtPokemons = (req, res, next) => {
 };
 
 exports.catchPokemon = (req, res, next) => {
-  const query_details = { 'id': req.params.id };
-  Pokemon.findOne(query_details, (err, pokemon) => {
-    if (err) {
-      return next(err);
-    } else {
+  const details = { 'id': req.params.id };
+  Pokemon.findOne(details)
+    .then(pokemon => {
       if (pokemon.catchInfo.isCaught){
         res.status(200).json({ message: 'Sorry, pokemon is caught' })
       } else {
         pokemon.catch(req.user._id);
         res.status(201).send(pokemon);
       }
-    }
-  });
+    })
+    .catch(err => next(err))
 };
--- AlenaYanova/app/routes/pokemons_routes.js
+++ AlenaYanova/app/routes/pokemons_routes.js
@@ -6,7 +6,7 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
   app.get('/pokemons/:id', PokemonsController.getPokemonById);
-  app.get('/pokemons/page/:page', PokemonsController.getPokemons, PokemonsController.pagination);
-  app.get('/pokemons/caught/page/:page', requireAuth, PokemonsController.getCaughtPokemons, PokemonsController.pagination);
+  app.get('/pokemons/page/:page', PokemonsController.setPokemonsQuery, PokemonsController.pagination);
+  app.get('/pokemons/caught/page/:page', requireAuth, PokemonsController.setCaughtPokemonsQuery, PokemonsController.pagination);
   app.post('/pokemons/:id/catch', requireAuth, PokemonsController.catchPokemon);
 };
