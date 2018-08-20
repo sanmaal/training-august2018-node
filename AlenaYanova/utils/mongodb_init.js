b8afa09863f6f4bccf6c84fdece53af9e3f8264a
Alena Ianova <alena_ianova@epam.com>
2018-08-20 21:24:15 +0300

    rewrite init database script with promises

--- AlenaYanova/utils/mongodb_init.js
+++ AlenaYanova/utils/mongodb_init.js
@@ -1,20 +1,27 @@
 const
   mongoose = require('mongoose'),
-  async = require('async'),
   url = require('../config/db').url,
   options = require('../config/db').options,
   Pokemon = require('../app/models/pokemon'),
   data = require('../pokemons.json');
 
-const openConnection = (cb) => {
+const openConnection = () => new Promise(resolve => {
   mongoose.connect(url, options)
     .then(() => {
       console.log('connected to database');
-      cb()
+      resolve();
     });
-};
+});
 
-const createPokemons = (cb) => {
+const removePokemons = () => new Promise(resolve => {
+  Pokemon.remove({})
+    .then(() => {
+      console.log('all existing pokemons removed');
+      resolve();
+    })
+});
+
+const createPokemons = () => new Promise(resolve => {
   let count = 0;
   data.pokemons.map((item) => {
     const pokemon = new Pokemon({
@@ -26,21 +33,20 @@ const createPokemons = (cb) => {
       count++;
       if (count === data.pokemons.length) {
         console.log('pokemons loaded');
-        cb();
+        resolve();
       }
     });
   });
-};
+});
 
-const closeConnection = () => {
+const closeConnection = () => new Promise(resolve => {
   mongoose.disconnect();
-  console.log('connection close')
-};
+  console.log('connection close');
+  resolve();
+});
 
-async.series(
-  [
-    openConnection,
-    createPokemons
-  ],
-  closeConnection
-);
+Promise.resolve()
+  .then(openConnection)
+  .then(removePokemons)
+  .then(createPokemons)
+  .then(closeConnection);
