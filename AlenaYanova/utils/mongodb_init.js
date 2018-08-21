a05d500e0dd1ee0686768706851621c654915814
Alena Ianova <alena_ianova@epam.com>
2018-08-21 19:30:49 +0300

    remove unnecessary promises in db init script

--- AlenaYanova/utils/mongodb_init.js
+++ AlenaYanova/utils/mongodb_init.js
@@ -9,29 +9,21 @@ const
   Pokemon = require('../app/models/pokemon'),
   data = require('../pokemons.json');
 
-const openConnection = () => new Promise(resolve => {
+const openConnection = () => (
   mongoose.connect(url, options)
     .then(() => {
       console.log('connected to database');
-      resolve();
     })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+);
 
-const removePokemons = () => new Promise(resolve => {
+const removePokemons = () => (
   Pokemon.remove({})
     .then(() => {
       console.log('all existing pokemons removed');
-      resolve();
     })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+);
 
-const createPokemons = () => new Promise(resolve => {
+const createPokemons = () => {
   let pokemons = [];
   data.pokemons.map(item => {
     const pokemon = new Pokemon({
@@ -40,24 +32,24 @@ const createPokemons = () => new Promise(resolve => {
     });
     pokemons.push(pokemon);
   });
-  Pokemon.collection.insert(pokemons)
+  return Pokemon.collection.insert(pokemons)
     .then(() => {
         console.log('pokemons loaded');
-        resolve();
       })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+};
 
-const closeConnection = () => new Promise(resolve => {
-  mongoose.disconnect();
-  console.log('connection close');
-  resolve();
-});
+const closeConnection = () => (
+  mongoose.disconnect()
+    .then(() => {
+      console.log('connection close');
+    })
+);
 
 Promise.resolve()
   .then(openConnection)
   .then(removePokemons)
   .then(createPokemons)
-  .then(closeConnection);
+  .then(closeConnection)
+  .catch((err) => {
+    console.log(err);
+  });
