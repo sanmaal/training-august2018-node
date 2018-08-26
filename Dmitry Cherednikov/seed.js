f63e31b669e2daa041a7432ad2ebc78d2160feca
Adore <hiroyukki@yandex.ru>
2018-08-27 02:59:13 +0300

    fixed path

--- Dmitry Cherednikov/seed.js
+++ Dmitry Cherednikov/seed.js
@@ -5,7 +5,7 @@ const { Pokemon } = require('./models/Pokemon.js');
 const { pokemons } = require('./pokemons.json');
 
 const seed = () => {
-	mongoose.connect(config.uri, { useNewUrlParser: true })
+	mongoose.connect(uri, { useNewUrlParser: true })
 		.then(() => Pokemon.deleteMany({}))
 		.then(() => Pokemon.insertMany(pokemons))
 		.then(() => mongoose.disconnect())
