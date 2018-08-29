935b6fb4123973653a4b0c77fd79cf4ba25cecd0
Sergei Gavrish <36244364+gogumaUno@users.noreply.github.com>
2018-08-29 14:26:57 +0300

    Delete db.js

--- sergei_gavrish/app/config/db.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-import  { USER, PASSWORD, HOST, DBPORT, NAME } from './config';
-
-mongoose.connect(`mongodb://${USER}:${PASSWORD}@${HOST}:${DBPORT}/${NAME}`, { useNewUrlParser: true });
