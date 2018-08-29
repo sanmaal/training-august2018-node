ee8d2ef25f420f8cefdb24a75a394162f04f700e
Sergei Gavrish <36244364+gogumaUno@users.noreply.github.com>
2018-08-29 14:26:37 +0300

    Update config.js

--- sergei_gavrish/app/config/config.js
+++ sergei_gavrish/app/config/config.js
@@ -1,7 +1,19 @@
 import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
 dotenv.config();
 
-const { USER, PASSWORD, HOST, DBPORT, NAME, KEY, SERVERPORT } = process.env;
+if (process.env.LOCAL_DB) {
+  const { DBPORT, NAME, KEY, SERVERPORT } = process.env;
+  const db = mongoose.connect(`mongodb://localhost:${DBPORT}/${NAME}`, { useNewUrlParser: true });
+  export { KEY, SERVERPORT, db };
+} else {
+  const { USER, PASSWORD, HOST, DBPORT, NAME, KEY, SERVERPORT } = process.env;
+  const db = mongoose.connect(`mongodb://${USER}:${PASSWORD}@${HOST}:${DBPORT}/${NAME}`, { useNewUrlParser: true });
+}
 
-export { USER, PASSWORD, HOST, DBPORT, NAME, KEY, SERVERPORT };
+export {
+  KEY,
+  SERVERPORT,
+  db,
+};
