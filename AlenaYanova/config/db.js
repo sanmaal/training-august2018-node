f64a0cde18873b404a53f74992d882f3fe96ff2e
Alena Ianova <alena_ianova@epam.com>
2018-09-04 00:15:11 +0300

    add different hosts for database

--- AlenaYanova/config/db.js
+++ AlenaYanova/config/db.js
@@ -1,8 +1,8 @@
 const { DB_USER, DB_PWD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 module.exports = {
-  url: (DB_USER && DB_PWD && DB_HOST)?
+  url: (DB_USER && DB_PWD)?
     `mongodb://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`:
-    `mongodb://localhost:${DB_PORT}/${DB_NAME}`,
+    `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   options: { useNewUrlParser: true }
 };
