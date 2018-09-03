90281ff2c847dbaa6537d6b8c244dc4f9bd5ed90
Alena Ianova <alena_ianova@epam.com>
2018-09-04 00:08:32 +0300

    add url for database on localhost

--- AlenaYanova/config/db.js
+++ AlenaYanova/config/db.js
@@ -1,6 +1,8 @@
 const { DB_USER, DB_PWD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 module.exports = {
-  url: `mongodb://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  url: (DB_USER && DB_PWD && DB_HOST)?
+    `mongodb://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`:
+    `mongodb://localhost:${DB_PORT}/${DB_NAME}`,
   options: { useNewUrlParser: true }
 };
